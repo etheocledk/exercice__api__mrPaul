0898802ce164c65e693a54ffa97a871fdd7ec1a1
Dieudonné <dieudonnepaqui@gmail.com>
2023-07-05 10:10:46 +0100

    correction of all files

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 async function blogLoader() {
     let link = window.location.pathname
+
     console.log(link);
     if (link === '/') {
+        //     let shoButton = document.querySelector('body')
+        // shoButton.innerHTML = `<img src="loader.gif" alt="Loader" width= 20px>`
         let showButton = document.querySelector('.posts_categories_content_type')
         showButton.innerHTML = `<img src="loader.gif" alt="Loader" width= 20px>`
         let response = await fetch(`https://test1.ecole229.bj/api/categories`)
         let dataCategories = await response.json()
         console.log(dataCategories)
         let showBtn = ``
+        
         if (dataCategories.status === true) {
             if (dataCategories.categories.length > 0) {
                 dataCategories.categories.forEach(item => {
@@ -33,18 +37,19 @@ async function blogLoader() {
                 if (dataPosts.posts.length > 0) {
 
                     let randomIndex = Math.floor((Math.random() * dataPosts.posts.length))
-                    randomNews = `<div class="banner_content">
-                    <div class="banner_content_left">
-                        <img src="https://test1.ecole229.bj${dataPosts.posts[randomIndex].image}" alt="">
-                    </div>
-                    <div class="banner_content_right">
-                        <div class="banner_content_right_author">
-                            <h4>${dataPosts.posts[randomIndex].user_name}</h4>
-                            <h4>${dataPosts.posts[randomIndex].published_at}</h4>
+                    randomNews = `
+                    <div class="banner_content">
+                        <div class="banner_content_left">
+                            <img src="https://test1.ecole229.bj${dataPosts.posts[randomIndex].image}" alt="">
                         </div>
-                        <p id="titre">${dataPosts.posts[randomIndex].title}</p>
-                    </div>
-                </div>`
+                        <div class="banner_content_right">
+                            <div class="banner_content_right_author">
+                                <h4>${dataPosts.posts[randomIndex].user_name}</h4>
+                                <h4>${dataPosts.posts[randomIndex].published_at}</h4>
+                            </div>
+                            <p id="titre">${dataPosts.posts[randomIndex].title}</p>
+                        </div>
+                    </div>`
 
                     if (randomNewsTag) {
                         randomNewsTag.insertAdjacentHTML('beforeend', randomNews)
@@ -55,35 +60,163 @@ async function blogLoader() {
 
 
                     let button = document.querySelectorAll('.posts_categories_content_type button')
+
+                    let showArticles = ``;
+                    dataPosts.posts.forEach(item => {
+                        showArticles += `
+                             <div class="post_articles_content_item">
+                                <div class="post_articles_content_item_img">
+                                    <img src="https://test1.ecole229.bj${item.image}" alt="">
+                                </div>
+                                <div class="post_articles_content_item_texte">
+                                    <p id="date"><span class="mdi mdi-calendar-month"></span> ${item.published_at}</p>
+                                    <h4 id="title">${item.title}</h4>
+                                    <a class='more' href="/detail.html?id=${item.id}">Voir plus...</a>
+                                </div>
+                            </div>`})
+
+                    showPosts.innerHTML = showArticles
+
                     console.log(button);
-                    
+
                     if (button) {
                         button.forEach(item => {
                             item.addEventListener('click', (e) => {
                                 e.preventDefault()
+                                showArticles = ``; 
                                 let postsFilter;
                                 if (showPosts) {
-                                    let showArticles = ``;
-                                    if(item.value==='All'){
-                                        postsFilter=dataPosts.posts
-                                    }else{
-                                        postsFilter=dataPosts.posts.filter((post) => post.category_name == item.value)
+                                   
+                                    if (item.value === 'All') {
+                                        postsFilter = dataPosts.posts
+                                        console.log(postsFilter.length);
+                                    } else {
+                                        postsFilter = dataPosts.posts.filter((post) => post.category_name == item.value)
+                                        console.log(postsFilter.length);
                                     }
+
+                                    let pageButton__container = document.querySelector('.pageButton__container')
+                                    if (postsFilter.length > 10) {
+                                        let pageButton = ``
+                                        let pageNumber = Math.ceil(postsFilter.length / 9)
+                                        for (let i = 1; i <= pageNumber; i++) {
+                                            pageButton += `<a href='#'>${i}</a>`
+                                        }
+                                        pageButton = `
+                                            <button id="preview"> &blacktriangleleft;</button>
+                                            ${pageButton}
+                                            <button class="next"> &blacktriangleright;</button>
+                                            `
+                                        pageButton__container.innerHTML = pageButton
+                                    } else {
+                                        pageButton__container.innerHTML = ``
+                                    }
+
+                                    let getButtonPagination = document.querySelectorAll('.pageButton__container *')
+
+                                    if (getButtonPagination) {
+                                        getButtonPagination.forEach(item => {
+
+                                            item.addEventListener('click', (e) => {
+                                                e.preventDefault()
+                                                let itemTarget = e.currentTarget /*renvoie l'element courant sur lequel on a cliqué*/
+                                                /* let eTarget=e.target //renvoi le dernier enfant de l'element sur lequel on a cliqué
+                                                console.log(itemTarget); */
+                                                if (showPosts.innerHTML != '') {
+                                                    showArticles = ``
+
+                                                    if (e.target.tagName === 'A') {
+                                                        getButtonPagination.forEach(item => {
+                                                            if (item.classList.contains('active')) {
+                                                                item.classList.remove('active')
+                                                            }
+                                                        })
+                                                        e.target.classList.add('active')
+                                                        
+                                                        let numPage = Number(e.target.innerText)
+
+                                                        
+                                                        let end = (numPage * 9)
+                                                        let begin = end - 9
+                                                        let viewArticle = postsFilter.slice(begin, end)
+                                                        console.log(viewArticle);
+                                                        viewArticle.forEach(item => {
+                                                            showArticles += `
+                                                                 <div class="post_articles_content_item">
+                                                                    <div class="post_articles_content_item_img">
+                                                                        <img src="https://test1.ecole229.bj${item.image}" alt="">
+                                                                    </div>
+                                                                    <div class="post_articles_content_item_texte">
+                                                                        <p id="date"><span class="mdi mdi-calendar-month"></span> ${item.published_at}</p>
+                                                                        <h4 id="title">${item.title}</h4>
+                                                                        <a class='more' href="/detail.html?id=${item.id}">Voir plus...</a>
+                                                                    </div>
+                                                                </div>`})
+
+                                                        showPosts.innerHTML = showArticles
+
+                                                    } else if (e.target.tagName === 'BUTTON') {
+                                                        let preview = document.querySelector('#preview')
+                                                        preview.addEventListener('click', () => {
+
+                                                            console.log((e.target));
+                                                            getButtonPagination.forEach(item => {
+                                                                if (item.tagName === 'A' && item.classList.contains('active')) {
+                                                                    item.classList.remove('active')
+                                                                    let numberA = Number(item.innerText)
+                                                                    let numPage;
+
+
+                                                                    if(numberA>1) {
+                                                                        numPage = numberA - 1}
+
+                                                                   
+                                                                    let end = (numPage * 9)
+                                                                    let begin = end - 9
+                                                                    let viewArticle = postsFilter.slice(begin, end)
+
+                                                                    viewArticle.forEach(item => {
+                                                                        showArticles += `
+                                                                            <div class="post_articles_content_item">
+                                                                                <div class="post_articles_content_item_img">
+                                                                                    <img src="https://test1.ecole229.bj${item.image}" alt="">
+                                                                                </div>
+                                                                                <div class="post_articles_content_item_texte">
+                                                                                    <p id="date"><span class="mdi mdi-calendar-month"></span> ${item.published_at}</p>
+                                                                                    <h4 id="title">${item.title}</h4>
+                                                                                    <a class='more' href="/detail.html?id=${item.id}">Voir plus...</a>
+                                                                                </div>
+                                                                            </div>`})
+
+                                                                    showPosts.innerHTML = showArticles
+                                                                }
+                                                            })
+
+                                                        })
+                                                    }
+                                                }
+
+                                            })
+                                        })
+                                    }
+
+
+
                                     postsFilter.forEach(item => {
-                                        showArticles += `
+                                                showArticles+= `
                                     <div class="post_articles_content_item">
                                         <div class="post_articles_content_item_img">
                                             <img src="https://test1.ecole229.bj${item.image}" alt="">
                                         </div>
                                         <div class="post_articles_content_item_texte">
-                                            <p id="title"><strong>Titre : </strong>  ${item.title}</p>
-                                            <p id="date"> <strong>Date : </strong> ${item.published_at}</p>
-                                            <a class='more' href="http://127.0.0.1:5500/detail.html?id=${item.id}">Voir plus...</a>
+                                            <p id="date"><span class="mdi mdi-calendar-month"></span> ${item.published_at}</p>
+                                            <h4 id="title">${item.title}</h4>
+                                            <a class='more' href="/detail.html?id=${item.id}">Voir plus...</a>
                                         </div>
                                     </div>`
                                     })
-                                    showPosts.innerHTML = 
-                                     showArticles
+
+                                    showPosts.innerHTML = showArticles 
                                 } else {
                                     showPosts.innerHTML = 'Aucune donnée disponible'
                                 }
@@ -91,13 +224,20 @@ async function blogLoader() {
                             })
                         })
                     }
-                } 
+                }
             }
 
         }
     }
 
-    if (link === '/detail.html') {
+    let numberofItem = 9
+
+    function showList() {
+
+    }
+
+
+    if (link ==='/detail.html') {
         let newLink = window.location.href
         let url = new URL(newLink)
         let id = url.searchParams.get('id')
